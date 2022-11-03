23455f31081f3ec46a3cb4cbaaafbfbd1e03da74
waccampos <114779612+waccampos@users.noreply.github.com>
2022-11-03 15:57:40 -0300

    Create areaMain.java

---
+++ areaMain.java
@@ -0,0 +1,19 @@
+package area;
+
+/**
+ *
+ * @author PICHAU
+ */
+public class Area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AreaGeo resultado = new AreaGeo();
+        resultado.area();
+    }
+    
+    
+}
